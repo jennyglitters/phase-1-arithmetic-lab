e061305e30d8236bb8c6f7b1c3dcc653610b372d
jennipher <voliduser@gmail.com>
2023-08-03 18:08:04 -0400

    arithmetic-labs

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
+let num1 = 2
+let num2 = 31
+console.log(num1 * num2);
 
-// Write your code here
+let random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+
+function mod(num3, num4) {
+    return num3 % num4;
+  }
+  
+  module.exports = mod;
+  
+  let num3 = 12;
+  let num4 = 8;
+  
+  let remainder = mod(num3, num4);
+  console.log(remainder);
+
+function max(numbers) {
+    let max = Math.max(...numbers);
+  return Math.max(...numbers);
+}
+
+max([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]);
--- test/indexTest.js
+++ test/indexTest.js
@@ -1,32 +1,41 @@
-require ( './helpers.js' );
-
-const chai = require('chai')
+const chai = require('chai');
 const expect = chai.expect;
+const mod = require('../index');
 
 describe('index.js', function() {
   describe('multiply', function() {
     it("is an equation whose multiplied results will equal 62", function() {
-      expect(num1).to.be.a('number')
-      expect(num2).to.be.a('number')
-      expect(multiply).to.eq(62)
+      let num1 = 2;
+      let num2 = 31;
+      let multiply = num1 * num2;
+      expect(num1).to.be.a('number');
+      expect(num2).to.be.a('number');
+      expect(multiply).to.eq(62);
     });
   });
+
   describe('random', function() {
     it("generates a random integer greater than 0", function() {
-      expect(Number.isInteger(random)).to.be.true
-      expect(random).to.not.eq(0)
+      expect(Number.isInteger(random)).to.be.true;
+      expect(random).to.not.eq(0);
     });
   });
-  describe('mod', function() {
-    it("is an equation that calculates a remainder that is equal to 4", function() {
-      expect(num3).to.be.a('number')
-      expect(num4).to.be.a('number')
-      expect(mod).to.eq(4)
+
+  describe('mod function', () => {
+    it('should calculate a remainder that is equal to 4', () => {
+      const num3 = 12;
+      const num4 = 8;
+      const expected = 4;
+      const actual = mod(num3, num4);
+      expect(actual).to.eq(expected);
     });
   });
+
   describe('max', function() {
     it("will return 20 as the highest number in the set", function() {
-      expect(max).to.eq(20)
+      let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+      let result = max(numbers);
+      expect(result).to.eq(20);
     });
   });
 });
